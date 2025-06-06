e43b7cfb12248e546a4af8825bd2f152f2af4f83
Amy <victorhugobaiajunqueira@gmail.com>
2025-06-05 22:16:26 -0300

    sistema

---
+++ sistema-api/src/main/java/turma/java/sistema_api/SistemaApiApplication.java
@@ -0,0 +1,13 @@
+package turma.java.sistema_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaApiApplication.class, args);
+	}
+
+}
---
+++ sistema-api/src/test/java/turma/java/sistema_api/SistemaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package turma.java.sistema_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
